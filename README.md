53db2e67a5585b1a56cbc9ad403491a9ffa69bce
Oliver Vartiainen <oliver@iki.fi>
2017-06-24 16:08:48 +0300

    Adhere to the LuaRocks style guide

--- README.md
+++ README.md
@@ -10,13 +10,13 @@ How to use
 Require the thing:
 
 ```lua
-local simple = require('naivebayes/simple')
+local Simple = require('naivebayes/simple')
 ```
 
 Instantiate a classifier:
 
 ```lua
-local classifier = simple.new()
+local classifier = Simple.new()
 ```
 
 Make the classifier learn stuff (replace `[data]` with a simple table and `[classification]` with whatever):
@@ -37,7 +37,7 @@ local guessedClassification = classifier:guess([data])
 Simple text analysis:
 
 ```lua
-local simple = require('naivebayes/simple')
+local Simple = require('naivebayes/simple')
 
 local function splitSentenceToWords(sentence)
   -- *snip*
@@ -46,7 +46,7 @@ end
 local positiveSentence = 'ice-cream is delicious and tasty'
 local negativeSentence = 'escargots are slimy and gross'
 
-local classifier = simple.new()
+local classifier = Simple.new()
 classifier:learn(splitSentenceToWords(positiveSentence), 'positive')
 classifier:learn(splitSentenceToWords(negativeSentence), 'negative')
 
--- tests/simple.lua
+++ tests/simple.lua
@@ -1,52 +1,52 @@
-require 'minctest'
+require "minctest"
 
-local simple = require('naivebayes/simple')
+local Simple = require("naivebayes/simple")
 
 local function splitToWords(sentence)
-  local words = {}
-  for word in string.gmatch(sentence, '%S+') do
-    table.insert(words, word)
-  end
+   local words = {}
+   for word in string.gmatch(sentence, "%S+") do
+      table.insert(words, word)
+   end
 
-  return words
+   return words
 end
 
 -- Test data to learn
 local sentences = {
-  ['heartwarming optimists achieved harmony'] = 'positive',
-  ['tranquil benefactors rejoiced in sunshine'] = 'positive',
-  ['menacing filth smells foul'] = 'negative',
-  ['sinister villains horrified the injured'] = 'negative'
+   ["heartwarming optimists achieved harmony"] = "positive",
+   ["tranquil benefactors rejoiced in sunshine"] = "positive",
+   ["menacing filth smells foul"] = "negative",
+   ["sinister villains horrified the injured"] = "negative"
 }
 
-local classifier = simple.new()
+local classifier = Simple.new()
 
 -- Split the test sentences to words and learn them
 for sentence, classification in pairs(sentences) do
-  classifier:learn(splitToWords(sentence), classification)
+   classifier:learn(splitToWords(sentence), classification)
 end
 
-lrun('test', function()
-  local words
+lrun("test", function()
+   local words
 
-  -- completely positive
-  words = splitToWords('tranquil optimists rejoiced')
-  lok(classifier:guess(words) == 'positive')
+   -- completely positive
+   words = splitToWords("tranquil optimists rejoiced")
+   lok(classifier:guess(words) == "positive")
 
-  -- mostly positive
-  words = splitToWords('heartwarming optimists were injured')
-  lok(classifier:guess(words) == 'positive')
+   -- mostly positive
+   words = splitToWords("heartwarming optimists were injured")
+   lok(classifier:guess(words) == "positive")
 
-  -- balanced
-  words = splitToWords('tranquil filth horrified sunshine ')
-  lok(classifier:guess(words) == 'positive'
-    or classifier:guess(words) == 'negative')
+   -- balanced
+   words = splitToWords("tranquil filth horrified sunshine ")
+   lok(classifier:guess(words) == "positive"
+      or classifier:guess(words) == "negative")
 
-  -- mostly negative
-  words = splitToWords('sinister harmony smells')
-  lok(classifier:guess(words) == 'negative')
+   -- mostly negative
+   words = splitToWords("sinister harmony smells")
+   lok(classifier:guess(words) == "negative")
 
-  -- completely negative
-  words = splitToWords('foul filth menacing the injured')
-  lok(classifier:guess(words) == 'negative')
+   -- completely negative
+   words = splitToWords("foul filth menacing the injured")
+   lok(classifier:guess(words) == "negative")
 end)
